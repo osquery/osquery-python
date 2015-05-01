61a4e45469613be7e308317814571552542a24f5
Mike Arpaia <mike@arpaia.co>
2015-05-01 09:06:28 -0700

    setup.py extra parameters and classifiers

--- setup.py
+++ setup.py
@@ -91,4 +91,22 @@ setup(name=TITLE,
       cmdclass={
           "generate": GenerateThriftCommand,
           "lint": LintCommand,
-      },)
+      },
+      classifiers=[
+          # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+          "Development Status :: 3 - Alpha",
+
+          "Intended Audience :: System Administrators",
+          "Topic :: Security",
+
+          'License :: OSI Approved :: BSD License',
+
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      keywords="security databases operating systems",)
