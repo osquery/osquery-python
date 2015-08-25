33c1d31ace4dfb7cf965a695db9dff62ce08a37f
Mike Arpaia <mike@arpaia.co>
2015-08-25 12:09:53 -0700

    Simple tool to run queries using the API
    ```
    $ sudo python examples/run.py "select * from time"
    ================================================================================
    hour => 12
    seconds => 39
    timestamp => Tue Aug 25 19:09:39 2015 UTC
    unix_time => 1440529779
    month => 8
    weekday => Tuesday
    year => 2015
    iso_8601 => 2015-08-25T19:09:39Z
    minutes => 9
    day => 25
    ================================================================================
    ```

---
+++ examples/run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+simple script which runs a query from stdin
+"""
+
+import sys
+import osquery
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: %s \"query\"" % sys.argv[0])
+        sys.exit(1)
+    CLIENT = osquery.ExtensionClient()
+    CLIENT.open()
+    RESULTS = CLIENT.extension_client().query(sys.argv[1])
+    if RESULTS.status.code != 0:
+        print("Error running the query: %s" % RESULTS.status.message)
+        sys.exit(1)
+
+    for row in RESULTS.response:
+        print("=" * 80)
+        for key, val in row.iteritems():
+            print("%s => %s" % (key, val))
+    if len(RESULTS.response) > 0:
+        print("=" * 80)
