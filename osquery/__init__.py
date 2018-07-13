f67a203d0a3bf5df9f4b1693c202b18c6df93f75
Nick Anderson <nanderson7@gmail.com>
2018-07-13 11:17:27 -0700

    version: bumping to 3.0.5 (#59)

--- osquery/__init__.py
+++ osquery/__init__.py
@@ -4,7 +4,7 @@ of patent rights can be found in the PATENTS file in the same directory.
 """
 
 __title__ = "osquery"
-__version__ = "3.0.4"
+__version__ = "3.0.5"
 __author__ = "osquery developers"
 __license__ = "BSD"
 __copyright__ = "Copyright 2015 Facebook"
